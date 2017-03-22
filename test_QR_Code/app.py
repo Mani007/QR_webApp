3ca0bed4293d446c87f37b518d9299fdafca5938
Mohnish <mohnish.a007@gmail.com>
2017-03-22 17:00:08 +0530

    QR code setup

--- test_QR_Code/app.py
+++ test_QR_Code/app.py
@@ -5,13 +5,19 @@ It contains the definition of routes and views for the application.
 
 from flask import Flask, render_template
 app = Flask(__name__)
+from route import *
+
+#app.debug = True
+wsgi_app = app.wsgi_app
 
 
-@app.route('/')
-def index():
-    """Renders a sample page."""
-    return render_template('index.html') 
 
 if __name__ == '__main__':
-   
-    app.run(debug = True)
+   import os
+   HOST = os.environ.get('SERVER_HOST', 'localhost')
+   try:
+      PORT = int(os.environ.get('SERVER_PORT', '5555'))
+   except ValueError:
+      PORT = 5555
+   app.run(HOST, PORT)
+    #app.run()
---
+++ test_QR_Code/route.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+from app import *
+import qrcode
+import qrtools
+
+img1 = qrcode.make('THE QR test')
+img = img1.save("C:/Users/madhu/Documents/GitHub/QRCode/qrflask/qrflask/static/images/myQR.png")
+
+@app.route('/')
+def index():
+    """Renders a sample page."""
+    return render_template('index.html') 
+
+@app.route('/QR')
+def qrcode():
+    """Renders a sample page."""
+    return render_template('index.html') 
